7e4ee6396f1cb2eaa7e9f95ca60c5575439ed72a
Oleksandr Bezhan <oleksandr.bezhan@gmail.com>
2018-09-13 19:22:41 +0300

    Hangman class. Mockable input and output streams.

--- src/main/kotlin/app/Hangman.kt
+++ src/main/kotlin/app/Hangman.kt
@@ -2,6 +2,8 @@ package app
 
 import org.slf4j.LoggerFactory
 import java.io.File
+import java.io.InputStream
+import java.io.PrintStream
 import java.util.*
 
 object Main {
@@ -9,56 +11,6 @@ object Main {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        fun round(knownWords: List<String>, without: List<Char>, letters: List<IndexedValue<Char?>>) {
-            println(letters.map { it.value ?: "_" }.joinToString(" "))
-            log.debug("[knownWords : ${knownWords.joinToString(" ")}]")
-            val unknownLetterIdx = letters.find { (_, c) -> c == null }?.index
-            if (unknownLetterIdx == null) {
-                println("done")
-            } else {
-                val wordsSubset = run {
-                    val knownLetters = letters.filter { it.value != null }
-                    val x = knownWords
-                            .filter { word -> knownLetters.forAll { word[it.index] == it.value } }
-                            .filter { word -> !without.exists { word.contains(it) } }
-                    log.debug("[wordsSubset: " + x.joinToString(" ") + "]")
-                    x
-                }
-                if (wordsSubset.isEmpty()) {
-                    println("I don't know this word. I give up. You won, congratulations!")
-                    return
-                }
-
-                val letter = run {
-                    val randomWord = wordsSubset[Random().nextInt(wordsSubset.size)]
-                    log.debug("[randomWord: $randomWord]")
-                    randomWord.toCharArray()[unknownLetterIdx]
-                }
-                println("$letter ? (yes/no)")
-                when (Scanner(System.`in`).next().trim()) {
-                    "yes" -> {
-                        val idx = run {
-                            tailrec fun readPos(): Int {
-                                print("Letter pos [1..${letters.size}]: ")
-                                val idx = Scanner(System.`in`).nextInt() - 1
-                                val alreadyHaveThisIdx = letters.filter { it.value != null }.map { it.index }.contains(idx)
-                                return if (idx < 0 || idx >= letters.size || alreadyHaveThisIdx) {
-                                    println("Wrong pos")
-                                    readPos()
-                                } else {
-                                    idx
-                                }
-                            }
-                            readPos()
-                        }
-                        round(wordsSubset, without, letters.set(idx, IndexedValue(idx, letter)))
-                    }
-                    "no" -> round(wordsSubset, without + letter, letters)
-                    else -> error("Illegal answer")
-                }
-            }
-        }
-
         println("Hi. Please, think of a word.")
         println("What is the word length ?")
         val knownWords = run {
@@ -70,7 +22,61 @@ object Main {
 
         val length = Scanner(System.`in`).nextInt()
         val letters = (0 until length).map { null }
-        round(knownWords.filter { word -> word.length == length }, emptyList(), letters.withIndex().toList())
+        Hangman(0, System.out, System.`in`).round(knownWords.filter { word -> word.length == length }, letters.withIndex().toList())
+    }
+}
+
+class Hangman(seed: Long?, private val printStream: PrintStream, private val inputStream: InputStream) {
+    private val random = seed?.let { Random(it) } ?: Random()
+
+    fun round(knownWords: List<String>, letters: List<IndexedValue<Char?>>, without: List<Char> = emptyList()) {
+        println(letters.map { it.value ?: "_" }.joinToString(" "))
+        Main.log.debug("[knownWords : ${knownWords.joinToString(" ")}]")
+        val unknownLetterIdx = letters.find { (_, c) -> c == null }?.index
+        if (unknownLetterIdx == null) {
+            printStream.println("done")
+        } else {
+            val wordsSubset = run {
+                val knownLetters = letters.filter { it.value != null }
+                val x = knownWords
+                        .filter { word -> knownLetters.forAll { word[it.index] == it.value } }
+                        .filter { word -> !without.exists { word.contains(it) } }
+                Main.log.debug("[wordsSubset: " + x.joinToString(" ") + "]")
+                x
+            }
+            if (wordsSubset.isEmpty()) {
+                printStream.println("I don't know this word. I give up. You won, congratulations!")
+                return
+            }
+
+            val letter = run {
+                val randomWord = wordsSubset[random.nextInt(wordsSubset.size)]
+                Main.log.debug("[randomWord: $randomWord]")
+                randomWord.toCharArray()[unknownLetterIdx]
+            }
+            printStream.println("$letter ? (yes/no)")
+            when (Scanner(inputStream).next().trim()) {
+                "yes" -> {
+                    val idx = run {
+                        tailrec fun readPos(): Int {
+                            print("Letter pos [1..${letters.size}]: ")
+                            val idx = Scanner(inputStream).nextInt() - 1
+                            val alreadyHaveThisIdx = letters.filter { it.value != null }.map { it.index }.contains(idx)
+                            return if (idx < 0 || idx >= letters.size || alreadyHaveThisIdx) {
+                                printStream.println("Wrong pos")
+                                readPos()
+                            } else {
+                                idx
+                            }
+                        }
+                        readPos()
+                    }
+                    round(wordsSubset, letters.set(idx, IndexedValue(idx, letter)), without)
+                }
+                "no" -> round(wordsSubset, letters, without + letter)
+                else -> error("Illegal answer")
+            }
+        }
     }
 }
 
